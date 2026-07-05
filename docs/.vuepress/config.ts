ebd02c4cdad5b2c16946b0e097f3528a135b34e3
aquars <2352935097@qq.com>
2026-07-05 22:59:54 +0800

    fix:修改了一些布局

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -68,6 +68,15 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
               },
             ],
           },
+          {
+            text: '其他技术文章',
+            items: [
+              { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
+              { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
+              { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
+              { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+            ],
+          }
         ],
       },
       {
@@ -78,13 +87,10 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         ],
       },
       {
-        text: '技术',
+        text: 'AI相关',
         link: '/technology/',
         items: [
-          { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-          { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-          { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-          { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+
         ],
       },
       {
---
+++ docs/03.AI相关/remotion动画.md
@@ -0,0 +1,12 @@
+---
+title: remotion动画
+date: 2026-07-05 22:38:13
+permalink: /pages/5ff67c/
+categories:
+  - AI相关
+tags:
+  - 
+author: 
+  name: aquars
+  link: https://github.com/43aquarius
+---
